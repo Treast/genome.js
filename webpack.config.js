df517bd13d47e874f783a7f3dbf9ce6c85ff8413
Vincent Riva <treast@macbook-pro-de-vincent.home>
2019-03-27 16:51:52 +0100

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const path = require('path');
+
+let optimization = {
+  minimizer: [],
+};
+
+let outputFileName = 'main.js';
+let mode = 'development';
+
+if (process.env.NODE_ENV === 'production') {
+  optimization.minimizer.push(new UglifyJsPlugin());
+  outputFileName = 'main.min.js';
+  mode = 'production';
+}
+
+const config = {
+  entry: path.resolve(__dirname, 'src', 'main.ts'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: outputFileName,
+    libraryTarget: 'umd',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        enforce: 'pre',
+        use: [
+          {
+            loader: 'tslint-loader',
+            options: {
+              "configFile": "tslint.json",
+              "tsConfigFile": "tsconfig.json",
+              "typeCheck": true
+            }
+          }
+        ],
+        exclude: /(node_modules|bower_components)/,
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/,
+      }
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
+  },
+  optimization,
+  mode
+};
+
+module.exports = config;
